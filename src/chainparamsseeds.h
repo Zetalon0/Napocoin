d0ab03ca4210839705c5764955fdd8de299f0cfa
Zetalon0 <113819153+Zetalon0@users.noreply.github.com>
2022-10-03 02:10:43 +1030

    Update chainparamsseeds.h

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -7,7 +7,9 @@
  * Each line contains a 16-byte IPv6 address and a port.
  * IPv4 as well as onion addresses are wrapped inside an IPv6 address accordingly.
  */
-static SeedSpec6 pnSeed6_main[] = {};
+static SeedSpec6 pnSeed6_main[] = {
+    {{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xff,0xff,0x26,0xf2,0x99,0xab}, 8712}
+};
 
 static SeedSpec6 pnSeed6_test[] = {};
 #endif // BITCOIN_CHAINPARAMSSEEDS_H
