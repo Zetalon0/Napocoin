01aad5ab31501a509c2e3a3cd7ba123c2e13a400
Zetalon0 <zetalon00@gmail.com>
2022-10-13 11:01:17 +1030

    Revert "Update net_processing.cpp"
    This reverts commit 221e5336199c3889741bd6300b442245180882de.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -3352,7 +3352,9 @@ bool PeerLogicValidation::ProcessMessages(CNode* pfrom, std::atomic<bool>& inter
     // Scan for message start
     if (memcmp(msg.hdr.pchMessageStart, chainparams.MessageStart(), CMessageHeader::MESSAGE_START_SIZE) == 0) {
         fMagic = false;
-    }  else {
+    } else if (memcmp(msg.hdr.pchMessageStart, chainparams.MessageStartOld(), CMessageHeader::MESSAGE_START_SIZE) == 0) {
+        fMagic = true;
+    } else {
         LogPrint(BCLog::NET, "PROCESSMESSAGE: INVALID MESSAGESTART %s peer=%d\n", SanitizeString(msg.hdr.GetCommand()), pfrom->GetId());
         pfrom->fDisconnect = true;
         return false;
@@ -3360,7 +3362,7 @@ bool PeerLogicValidation::ProcessMessages(CNode* pfrom, std::atomic<bool>& inter
 
     // Read header
     CMessageHeader& hdr = msg.hdr;
-    bool validHeader = fMagic ? hdr.IsValid(chainparams.MessageStart());
+    bool validHeader = fMagic ? hdr.IsValid(chainparams.MessageStartOld()) : hdr.IsValid(chainparams.MessageStart());
     if (!validHeader)
     {
         LogPrint(BCLog::NET, "PROCESSMESSAGE: ERRORS IN HEADER %s peer=%d\n", SanitizeString(hdr.GetCommand()), pfrom->GetId());
